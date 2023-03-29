54e709e44a0f4878d251c6a3e92d265201f06324
maciek zelek <mzelechowski79@gmail.com>
2023-03-29 19:26:37 +0200

    exception

--- src/Excep01.java
+++ src/Excep01.java
@@ -1,7 +1,13 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Excep01 {
     public static void main(String[] args) throws IllegalAccessException {
 
-        System.out.println(Excep01.getNumberOfSeconds(-60));
+      //  System.out.println(Excep01.getNumberOfSeconds(-60));
+         //getPageByUrl();
+         getResult(3,0);
 
     }
     public static int getNumberOfSeconds(int hour) throws IllegalAccessException {
@@ -9,5 +15,24 @@ public class Excep01 {
             throw new IllegalAccessException("Hour must be >=0"+hour);
         }
         return hour*60*60;
+    }
+    private static void getResult(int x, int y){
+        try {
+            System.out.println(x / y);
+        } catch (ArithmeticException e){
+            System.out.println("Dzielisz przez zero "+e.getMessage());
+        }
+    }
+
+    private static void getPageByUrl(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj adres URL:  ");
+        try {
+            URL url = new URL(scanner.nextLine());
+        } catch (MalformedURLException e) {
+            //e.printStackTrace();
+            System.out.println("Podałes niepoprawy adres url. Jeszcze raz.");
+            getPageByUrl();
+        }
     }
 }
